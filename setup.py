4b8ce7c091465da5539aece27e1b0884be7b6f2d
Talens <talens@strw.leidenuniv.nl>
2017-06-18 13:55:06 -0400

    Changed from bringreduce to lsreduce.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='bringreduce',
-      version='2017.00',
-      description='bRING reduction code',
-      url='https://github.com/BringLeiden/bringreduce.git',
+setup(name='lsreduce',
+      version='2017.06',
+      description='La Silla reduction code',
+      url='https://gitlab.strw.leidenuniv.nl/talens/lsreduce.git',
       author='Talens',
       author_email='talens@strw.leidenuniv.nl',
-      packages=['bringreduce'],
-      scripts=['scripts/bringreduce.py'],
+      packages=['lsreduce'],
+      scripts=['scripts/lsreduce.py'],
       zip_safe=False)
